3ae7a12d75933539da1276e3a06c13fdb2a11118
amin bhs <amin.diez@gmail.com>
2017-07-12 20:32:40 +0200

    ajout des dconditions de redirection

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -11,4 +11,13 @@ class Robot
   def bearing
     @direction
   end
+
+  def turn_right
+    @direction = case @direction
+    when :north then  :east
+    when :south then :west
+    when :east  then :south
+    when :west  then :north
+    end
+  end
 end
