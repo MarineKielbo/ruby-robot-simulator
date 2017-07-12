4381b6b460eaf421f6d60e922b61126734d1ceef
Vincent Cornière <vincent.corniere@gmail.com>
2017-07-12 20:18:11 +0200

    add raise ArgumentError

--- robot_simulator.rb
+++ robot_simulator.rb
@@ -2,6 +2,9 @@ class Robot
   attr_reader :direction
 
   def orient(direction)
+    unless [:east, :west, :north, :south].include?(direction)
+      return raise ArgumentError
+    end
     @direction = direction
   end
 
